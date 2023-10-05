113ea9c683d6f35c3df0d3ae96ccb0caa0272eff
André Moraes Filho <softdev.andre@gmail.com>
2023-10-05 12:40:43 -0300

    :sparkles: created condition to allow screen changes on state change

--- app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
+++ app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
@@ -88,6 +88,22 @@ fun ReplyHomeScreen(
         navigationItemContentList = navigationItemContentList,
         modifier = modifier
     )
+
+    if (replyUiState.isShowingHomepage) {
+        ReplyAppContent(
+            replyUiState = replyUiState,
+            onTabPressed = onTabPressed,
+            onEmailCardPressed = onEmailCardPressed,
+            navigationItemContentList = navigationItemContentList,
+            modifier = Modifier
+        )
+    } else {
+        ReplyDetailsScreen(
+            replyUiState = replyUiState,
+            onBackPressed = onDetailScreenBackPressed,
+            modifier = Modifier
+        )
+    }
 }
 
 @Composable
